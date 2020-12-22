3c229fab7185dd9fbde45ed2d504512814e0b9e0
aliabdulazeez <aliabdulazeez01@gmail.com>
2020-12-22 18:36:52 +0900

    color removed from link ali

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -4,7 +4,7 @@ function Heading(){
     return <div className='title'>
              <h1>emojimedia</h1>
              <h6>A Complete List Of GitHub Markdown Emoji Markup</h6>
-             <h6>By <a style={{color: 'black'}} style={{textDecoration: 'none'}} href='https://github.com/aliabdulazeez'>Ali Adam</a></h6>
+             <h6>By <a  style={{textDecoration: 'none'}} href='https://github.com/aliabdulazeez'>Ali Adam</a></h6>
            </div> 
 };
 
