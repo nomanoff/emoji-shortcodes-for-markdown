bc367f866e2803cff5a10d006e89ec533832e747
aliabdulazeez <aliabdulazeez01@gmail.com>
2020-12-22 21:57:12 +0900

    100 so far

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -1,95 +1,1066 @@
 const emojipedia = [
+
+  //SMILING FACE
+
     {
       id: 1,
-      emoji: "💪",
-      name: "Tense Biceps",
-      markdown: ":muscle:"
+      emoji: "😀",
+      name: "Grinning Face",
+      markdown: ":grinning:"
     },
     {
       id: 2,
-      emoji: "🙏",
-      name: "Person With Folded Hands",
-      markdown: ":pray:"
+      emoji: "😃",
+      name: "Smiling Face With Open Mouth",
+      markdown: ":smiley:"
     },
     {
       id: 3,
-      emoji: "🤣",
-      name: "Rolling On The Floor, Laughing",
-      markdown: ":rofl:"
+      emoji: "😄",
+      name: "Smiling Face With Open Mouth & Smiling Eyes",
+      markdown: ":smile:"
     },
     {
       id: 4,
-      emoji: "😅",
-      name: "Smiling With Open Mouth & Sweat ",
-      markdown:":sweat_smile:"
-    },
-    {
-      id: 5,
-      emoji: "😎",
-      name: " Face With Sunglasses",
-      markdown:":sunglasses:"
-    },
-  
-    {
-      id: 6,
-      emoji: "🤨",
-      name: "Face With a Raised Eyebrow",
-      markdown:":raised_eyebrow:"
-    },
-    {
-      id: 7,
-      emoji: "😏",
-      name: "Smirking Face",
-      markdown:":smirk:"
-    },
-    {
-      id: 8,
-      emoji: "🤦‍♂️",
-      name: "Man Grabbing His Head",
-      markdown:":man:"
-    },
-    {
-      id: 9,
       emoji: "😁",
       name: "Grinning Face With Smiling Eyes",
       markdown:":grin:"
     },
     {
+      id: 5,
+      emoji: "😆",
+      name: "Smiling Face With Open Mouth & Closed Eyes",
+      markdown:":laughing:"
+    },
+  
+    {
+      id: 6,
+      emoji: "😅",
+      name: "Smiling Face With Open Mouth & Cold Sweat",
+      markdown:":sweat_smile:"
+    },
+    {
+      id: 7,
+      emoji: "😂",
+      name: "Face With Tears of Joy",
+      markdown:":joy:"
+    },
+    {
+      id: 8,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      markdown:":rofl:"
+    },
+    {
+      id: 9,
+      emoji: "🙂",
+      name: "Slightly Smiling Face",
+      markdown:":grin:"
+    },
+    {
       id: 10,
+      emoji: "🙃",
+      name: "Upside-Down Face",
+      markdown:":upside_down_face:"
+    },
+    {
+      id: 11,
       emoji: "😉",
       name: "Winking Face",
       markdown:":wink:"
     },
     {
-      id: 11,
+      id: 12,
+      emoji: "😊",
+      name: "Smiling Face With Smiling Eyes",
+      markdown:":blush:"
+    },
+    {
+      id: 13,
+      emoji: "😇",
+      name: "Smiling Face With Halo",
+      markdown:":innocent:"
+    },
+
+    // FACE AFFECTION
+
+    {
+      id: 14,
+      emoji: "😍",
+      name: "Smiling Face With Heart-Eyes",
+      markdown:":heart_eyes:"
+    },
+    {
+      id: 15,
+      emoji: "🥰",
+      name: "Smiling face with smiling eyes and three hearts",
+      markdown:":smiling_face_with_three_hearts:"
+    },
+
+    {
+      id: 16,
+      emoji: "😘",
+      name: "Face Throwing a Kiss",
+      markdown:":kissing_heart:"
+    },
+    {
+      id: 17,
+      emoji: "😗",
+      name: "Kissing Face",
+      markdown:":kissing:"
+    },
+    {
+      id: 18,
+      emoji: "😙",
+      name: "Kissing Face With Smiling Eyes",
+      markdown:":kissing_smiling_eyes:"
+    },
+    {
+      id: 19,
+      emoji: "😚",
+      name: "Kissing Face With Closed Eyes",
+      markdown:":kissing_closed_eyes:"
+    },
+    {
+      id: 20,
+      emoji: "🤩",
+      name: "Smirking Face With Starry Eyes",
+      markdown:":star_struck:"
+    },
+    {
+      id: 21,
+      emoji: "😊",
+      name: "Smiling Face",
+      markdown:":relaxed:"
+    },
+
+    // FACE TONGUE
+
+    {
+      id: 22,
+      emoji: "😋",
+      name: " Face Savouring Delicious Food",
+      markdown:":yum:"
+    },
+    {
+      id: 23,
+      emoji: "😛",
+      name: " Face With Stuck-Out Tongue",
+      markdown:":stuck_out_tongue:"
+    },
+    {
+      id: 24,
+      emoji: "😜",
+      name: " Face With Stuck-Out Tongue",
+      markdown:":stuck_out_tongue_winking_eye:"
+    },
+    {
+      id: 25,
+      emoji: "🤪",
+      name: "Crazy Face",
+      markdown:":zany_face:"
+    },
+    {
+      id: 26,
+      emoji: "😝",
+      name: "Face With Stuck-Out Tongue & Tightly Closed Eyes",
+      markdown:":stuck_out_tongue_closed_eyes:"
+    },
+    {
+      id: 27,
+      emoji: "🤑",
+      name: "Money-Mouth Face",
+      markdown:":money_mouth_face:"
+    },
+
+    //FACE HAND
+
+    {
+      id: 28,
+      emoji: "🤗",
+      name: "Hugging Face",
+      markdown:":hugs:"
+    },
+    {
+      id: 29,
+      emoji: "🤭",
+      name: "Face With Hand Over The Mouth",
+      markdown:":hand_over_mouth:"
+    },
+    {
+      id: 30,
+      emoji: "🤫",
+      name: "Exhorting Face",
+      markdown:":shushing_face:"
+    },
+    {
+      id: 31,
+      emoji: "🤔",
+      name: "Thinking Face",
+      markdown:":thinking:"
+    },
+
+//NEUTRAL AND SKEPTICAL FACE
+
+    {
+      id: 32,
+      emoji: "🤐",
+      name: "Zipper-Mouth Face",
+      markdown:":zipper_mouth_face:"
+    },
+    {
+      id: 33,
+      emoji: "😐",
+      name: "Neutral Face",
+      markdown:":neutral_face:"
+    },
+    {
+      id: 34,
+      emoji: "😶",
+      name: "Face Without Mouth",
+      markdown:":no_mouth:"
+    },
+    {
+      id: 35,
+      emoji: "😒",
+      name: "Unamused Face",
+      markdown:":unamused:"
+    },
+    {
+      id: 36,
+      emoji: "😬",
+      name: "Grimacing Face",
+      markdown:":grimacing:"
+    },
+    {
+      id: 37,
+      emoji: "🤨",
+      name: "Face With a Raised Eyebrow",
+      markdown:":raised_eyebrow:"
+    },
+    {
+      id: 38,
+      emoji: "😑",
+      name: " Expressionless Face",
+      markdown:":expressionless:"
+    },
+    {
+      id: 39,
+      emoji: "😏",
+      name: "Smirking/Whimsical Face",
+      markdown:":smirk:"
+    },
+    {
+      id: 40,
+      emoji: "🙄",
+      name: "Face With Rolling Eyes",
+      markdown:":roll_eyes:"
+    },
+    {
+      id: 41,
+      emoji: "🤥",
+      name: "Lying Face",
+      markdown:":lying_face:"
+    },
+
+    //SLEEPY FACE
+
+    {
+      id: 42,
+      emoji: "😌",
+      name: "Relieved Face",
+      markdown:":relieved:"
+    },
+    {
+      id: 43,
+      emoji: "😔",
+      name: "Pensive Face",
+      markdown:":pensive:"
+    },
+    {
+      id: 44,
+      emoji: "😪",
+      name: "Sleepy Face",
+      markdown:":sleepy:"
+    },
+    {
+      id: 45,
+      emoji: "🤤",
+      name: "Drooling Face",
+      markdown:":drooling_face:"
+    },
+    {
+      id: 46,
+      emoji: "😴",
+      name: "Sleeping Face",
+      markdown:":sleeping:"
+    },
+
+    //FACE UNWELL
+
+    {
+      id: 47,
+      emoji: "😷",
+      name: "Face With Medical Mask",
+      markdown:":mask:"
+    },
+    {
+      id: 48,
+      emoji: "🤒",
+      name: "",
+      markdown:":face_with_thermometer:"
+    },
+    {
+      id: 49,
+      emoji: "🤕",
+      name: "Face With Thermometer",
+      markdown:":face_with_head_bandage:"
+    },
+    {
+      id: 50,
+      emoji: "🤢",
+      name: "Nauseated Face",
+      markdown:":nauseated_face:"
+    },
+    {
+      id: 51,
+      emoji: "🤮",
+      name: "Face With Open Mouth, Throwing Up",
+      markdown:":vomiting_face:"
+    },
+    {
+      id: 52,
+      emoji: "🤧",
+      name: "Sneezing Face",
+      markdown:":sneezing_face:"
+    },
+    {
+      id: 53,
+      emoji: "🥵",
+      name: "Overheated face",
+      markdown:":hot_face:"
+    },
+    {
+      id: 54,
+      emoji: "🥶",
+      name: "Freezing face",
+      markdown:":cold_face:"
+    },
+    {
+      id: 55,
+      emoji: "🥴",
+      name: "Face With Unequal Eyes & Wavy Mouth",
+      markdown:":woozy_face:"
+    },
+    {
+      id: 56,
+      emoji: "😵",
+      name: "Dizzy Face",
+      markdown:":dizzy_face:"
+    },
+    {
+      id: 57,
+      emoji: "🤯",
+      name: "Face With Exploding Head",
+      markdown:":exploding_head:"
+    },
+
+    //FACE WITH A HAT
+
+    {
+      id: 58,
+      emoji: "🤠",
+      name: "Face With Cowboy Hat",
+      markdown:":cowboy_hat_face:"
+    },
+    {
+      id: 59,
+      emoji: "🥳",
+      name: "Face With Party Blower & Party Hat",
+      markdown:":partying_face:"
+    },
+
+    //FACE WITH GLASSES
+
+    {
+      id: 60,
+      emoji: "😎",
+      name: "Face With Sunglasses",
+      markdown:":sunglasses:"
+    },
+    {
+      id: 61,
+      emoji: "🤓",
+      name: "Nerd Face",
+      markdown:":nerd_face:"
+    },
+    {
+      id: 62,
+      emoji: "🧐",
+      name: "Face With Monocle",
+      markdown:":monocle_face:"
+    },
+
+    //FACE CONCERNED
+
+    {
+      id: 63,
+      emoji: "😕",
+      name: "Confused Face",
+      markdown:":confused:"
+    },
+    {
+      id: 64,
+      emoji: "😟",
+      name: "Worried Face",
+      markdown:":worried:"
+    },
+    {
+      id: 65,
+      emoji: "🙁",
+      name: "Slightly frowning face",
+      markdown:":slightly_frowning_face:"
+    },
+    {
+      id: 66,
+      emoji: "☹️",
+      name: "Frowning Face",
+      markdown:":frowning_face:"
+    },
+    {
+      id: 67,
+      emoji: "😮",
+      name: "Face With Open Mouth",
+      markdown:":open_mouth:"
+    },
+    {
+      id: 68,
+      emoji: "😯",
+      name: "Speechless Face",
+      markdown:":hushed:"
+    },
+    {
+      id: 69,
+      emoji: "😲",
+      name: "Astonished Face",
+      markdown:":astonished:"
+    },
+    {
+      id: 70,
+      emoji: "😳",
+      name: "Flushed Face",
+      markdown:":flushed:"
+    },
+    {
+      id: 71,
       emoji: "🥺",
-      name: "Face with begging eyes",
+      name: "Face With Begging Eyes",
       markdown:":pleading_face:"
     },
     {
-      id: 12,
+      id: 72,
+      emoji: "😦",
+      name: "Frowning Face With Open Mouth",
+      markdown:":frowning:"
+    },
+    {
+      id: 73,
+      emoji: "😧",
+      name: "Anguished Face",
+      markdown:":anguished:"
+    },
+    {
+      id: 74,
+      emoji: "😨",
+      name: "Fearful Face",
+      markdown:":fearful:"
+    },
+    {
+      id: 75,
+      emoji: "😰",
+      name: "Face With Open Mouth & Cold Sweat",
+      markdown:":cold_sweat:"
+    },
+    {
+      id: 76,
+      emoji: "😥",
+      name: "",
+      markdown:":disappointed_relieved:"
+    },
+    {
+      id: 77,
+      emoji: "😢",
+      name: "Disappointed but Relieved Face",
+      markdown:":cry:"
+    },
+    {
+      id: 78,
       emoji: "😭",
       name: "Loudly Crying Face",
       markdown:":sob:"
     },
     {
-      id: 13,
+      id: 79,
       emoji: "😱",
-      name: "Face Screaming in Fear",
+      name: " Face Screaming in Fear",
       markdown:":scream:"
     },
     {
-      id: 14,
-      emoji: "🤔",
-      name: "Thinking Face",
-      markdown:":thinking:"
+      id: 80,
+      emoji: "😖",
+      name: "Confounded Face",
+      markdown:":confounded:"
     },
     {
-      id: 15,
-      emoji: "😬",
-      name: "Grimacing Face",
-      markdown:":grimacing:"
+      id: 81,
+      emoji: "😣",
+      name: "Suffering Face",
+      markdown:":persevere:"
     },
+    {
+      id: 82,
+      emoji: "😞",
+      name: "Disappointed Face",
+      markdown:":disappointed:"
+    },
+    {
+      id: 83,
+      emoji: "😓",
+      name: "Face With Cold Sweat",
+      markdown:":sweat:"
+    },
+    {
+      id: 84,
+      emoji: "😩",
+      name: "Weary Face",
+      markdown:":weary:"
+    },
+    {
+      id: 85,
+      emoji: "😫",
+      name: "Tired Face",
+      markdown:":tired_face:"
+    },
+    {
+      id: 86,
+      emoji: "🥱",
+      name: "Yawning Face",
+      markdown:":yawning_face:"
+    },
+
+    //NEGATIVE FACE
+
+    {
+      id: 87,
+      emoji: "😤",
+      name: "Face With Look of Triumph",
+      markdown:":triumph:"
+    },
+    {
+      id: 88,
+      emoji: "😠",
+      name: "Angry Face",
+      markdown:":angry:"
+    },
+    {
+      id: 89,
+      emoji: "😡",
+      name: "Pouting Face",
+      markdown:":rage:"
+    },
+    {
+      id: 90,
+      emoji: "🤬",
+      name: "Face With Symbols Over The Mouth",
+      markdown:":cursing_face:"
+    },
+    {
+      id: 91,
+      emoji: "👿",
+      name: "Goblin",
+      markdown:":imp:"
+    },
+    {
+      id: 92,
+      emoji: "😈",
+      name: "Smiling Face With Horns",
+      markdown:":smiling_imp:"
+    },
+    {
+      id: 93,
+      emoji: "💀",
+      name: "Skull",
+      markdown:":skull:"
+    },
+    {
+      id: 94,
+      emoji: "☠️",
+      name: " Skull and Crossbones",
+      markdown:":skull_and_crossbones:"
+    },
+
+    //FACE COSTUMES
+
+    {
+      id: 95,
+      emoji: "💩",
+      name: "Pile of Poo",
+      markdown:":poop:"
+    },
+    {
+      id: 96,
+      emoji: "🤡",
+      name: "Clown Face",
+      markdown:":clown_face:"
+    },
+    {
+      id: 97,
+      emoji: "👻",
+      name: "Happy/Smiling Ghost",
+      markdown:":ghost:"
+    },
+    {
+      id: 98,
+      emoji: "👽",
+      name: "Alien",
+      markdown:":alien:"
+    },
+    {
+      id: 99,
+      emoji: "👾",
+      name: "Space Invader",
+      markdown:":space_invader:"
+    },
+    {
+      id: 100,
+      emoji: "🤖",
+      name: "Robot Face",
+      markdown:":robot:"
+    },
+
+    //CAT FACE
+
+    {
+      id: 101,
+      emoji: "",
+      name: "OTHERS ARE COMING SOON!",
+      markdown:""
+    },
+    {
+      id: 102,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 103,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 104,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 105,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 106,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 107,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 108,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 109,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 110,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 111,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 112,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 113,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 114,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 115,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 116,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 117,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 118,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 119,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 120,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 121,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 122,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 123,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 124,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 125,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 126,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 127,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 128,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 129,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 130,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 131,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 132,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 133,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 134,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 135,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 136,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 137,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 138,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 139,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 140,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 141,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 142,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 143,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 144,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 145,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 146,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 147,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 148,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 149,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 150,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 151,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 152,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 153,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 154,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 155,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 156,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 157,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 158,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 159,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 160,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 161,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 162,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 163,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 164,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id:165 ,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 166,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 167,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 168,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 169,
+      emoji: "",
+      name: "",
+      markdown:""
+    },
+    {
+      id: 170,
+      emoji: "",
+      name: "",
+      markdown:""
+    }
+
   
   ];
   
