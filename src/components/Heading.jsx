361b99e8973b7a384cd40bbf6380bd539e2452dd
aliabdulazeez <aliabdulazeez01@gmail.com>
2020-12-22 19:09:38 +0900

    title changed

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Heading(){
     return <div className='title'>
              <h1>emojimedia</h1>
-             <h6>A Complete List Of GitHub Markdown Emoji Markup</h6>
-             <h6>By <a  style={{textDecoration: 'none'}} href='https://github.com/aliabdulazeez'>Ali Adam</a></h6>
+             <h6>A List Of GitHub Markdown Emojis</h6>
+             <h6>By <a style={{textDecoration: 'none'}} href='https://github.com/aliabdulazeez'>Ali Adam</a></h6>
            </div> 
 };
 
