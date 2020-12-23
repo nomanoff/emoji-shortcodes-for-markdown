c2af1010d564b36c07ffb7f62c41bcae9f44c681
aliabdulazeez <aliabdulazeez01@gmail.com>
2020-12-23 21:24:11 +0900

    version 2.0

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -5,17 +5,24 @@ import useClipboard from "react-use-clipboard";
 function Entry(props){
 
 
-    const [isCopied, setCopied] = useClipboard(props.markdown);
+    const [isCopied, setCopied] = useClipboard(props.markdown, {
+      successDuration: 300,
+    });
 
+    
   
       return(
         
           <div  onClick={setCopied} className="emoji-card">
               
-                <p className="emoji">{props.img}</p>
+                
               
               <div>
-                {isCopied ? <p className="copied">copied</p> : null}
+                {isCopied ? null : <p className="emoji">{props.img}</p>}
+              </div>
+
+              <div>
+                {isCopied ? <p className="copied"><i className="fas fa-clipboard"></i></p> : null}
               </div>
                   
   
